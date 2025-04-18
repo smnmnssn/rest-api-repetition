99e4d7dd84a05c71ec622da28c789660e2b7cf6e
Simon Mansson <simonmansson@live.se>
2025-04-18 22:39:48 +0200

    added carData and loadCars function

---
+++ src/data/carData.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { Car } from "../models/Car";
+
+const DB_PATH = path.join(__dirname, "./db.json");
+
+export async function loadCars(): Promise <Car[]>  {
+    try { 
+        const data = await fs.promises.readFile(DB_PATH, "utf-8");
+        return JSON.parse(data);
+
+
+    } catch (error) {
+        console.log("Could not load cars");
+        return [];
+    }
+}
