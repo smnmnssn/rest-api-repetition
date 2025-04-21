7ed988534b0ab929203efa5ca5f25375ec80c7ce
Simon <simonmansson@live.se>
2025-04-21 17:17:37 +0200

    Updated database quite a bit, worked on main woops

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -1,9 +1,6 @@
 import { Request, Response } from "express";
 import { loadCars, saveCars } from "../data/carData";
 import { Car } from "../models/Car";
-// Här lägger du funktionerna för GET, POST, PUT, DELETE.
-
-let cars: Car[] = [];
 
 // GET endpoint för att HÄMTA ALLA BILAR.
 export const getAllCars = async (req: Request, res: Response) => {
@@ -18,7 +15,7 @@ export const getCarById = async (req: Request, res: Response) => {
   const car = cars.find((c) => c.id === id);
 
   if (!car) {
-   return res.status(404).json({ message: "Car not found!" });
+    return res.status(404).json({ message: "Car not found!" });
   }
 
   return res.status(200).json(car);
@@ -27,7 +24,21 @@ export const getCarById = async (req: Request, res: Response) => {
 // POST endpoint för att LÄGGA TILL BIL.
 export const addNewCar = async (req: Request, res: Response) => {
   const cars = await loadCars();
-  const { brand, model, year, type } = req.body;
+  const {
+    brand,
+    model,
+    yearRange,
+    bodyType,
+    horsepower,
+    torque,
+    transmission,
+    drivetrain,
+    fuelEconomy,
+    doors,
+    price,
+    engine,
+    cylinders,
+  } = req.body;
 
   try {
     if (
@@ -35,10 +46,28 @@ export const addNewCar = async (req: Request, res: Response) => {
       typeof brand !== "string" ||
       !model ||
       typeof model !== "string" ||
-      !year ||
-      typeof year !== "number" ||
-      !type ||
-      typeof type !== "string"
+      !yearRange ||
+      typeof yearRange !== "string" ||
+      !bodyType ||
+      typeof bodyType !== "string" ||
+      !horsepower ||
+      typeof horsepower !== "string" ||
+      !torque ||
+      typeof torque !== "string" ||
+      !transmission ||
+      typeof transmission !== "string" ||
+      !drivetrain ||
+      typeof drivetrain !== "string" ||
+      !fuelEconomy ||
+      typeof fuelEconomy !== "string" ||
+      !doors ||
+      typeof doors !== "string" ||
+      !price ||
+      typeof price !== "string" ||
+      !engine ||
+      typeof engine !== "string" ||
+      !cylinders ||
+      typeof cylinders !== "string"
     ) {
       return res
         .status(400)
@@ -49,8 +78,17 @@ export const addNewCar = async (req: Request, res: Response) => {
       id: String(Date.now()),
       brand,
       model,
-      year,
-      type,
+      yearRange,
+      bodyType,
+      horsepower,
+      torque,
+      transmission,
+      drivetrain,
+      fuelEconomy,
+      doors,
+      price,
+      engine,
+      cylinders,
     };
 
     cars.push(newCar);
@@ -63,11 +101,25 @@ export const addNewCar = async (req: Request, res: Response) => {
   }
 };
 
-// PUT endpoint för att UPDDATERA/ÄNDRA EN SPECIFIK BIL.
+// PUT endpoint för att UPPDATERA EN SPECIFIK BIL.
 export const updateCar = async (req: Request, res: Response) => {
   const cars = await loadCars();
   const { id } = req.params;
-  const { brand, model, year, type } = req.body;
+  const {
+    brand,
+    model,
+    yearRange,
+    bodyType,
+    horsepower,
+    torque,
+    transmission,
+    drivetrain,
+    fuelEconomy,
+    doors,
+    price,
+    engine,
+    cylinders,
+  } = req.body;
 
   try {
     if (
@@ -75,17 +127,35 @@ export const updateCar = async (req: Request, res: Response) => {
       typeof brand !== "string" ||
       !model ||
       typeof model !== "string" ||
-      !year ||
-      typeof year !== "number" ||
-      !type ||
-      typeof type !== "string"
+      !yearRange ||
+      typeof yearRange !== "string" ||
+      !bodyType ||
+      typeof bodyType !== "string" ||
+      !horsepower ||
+      typeof horsepower !== "string" ||
+      !torque ||
+      typeof torque !== "string" ||
+      !transmission ||
+      typeof transmission !== "string" ||
+      !drivetrain ||
+      typeof drivetrain !== "string" ||
+      !fuelEconomy ||
+      typeof fuelEconomy !== "string" ||
+      !doors ||
+      typeof doors !== "string" ||
+      !price ||
+      typeof price !== "string" ||
+      !engine ||
+      typeof engine !== "string" ||
+      !cylinders ||
+      typeof cylinders !== "string"
     ) {
       return res
         .status(400)
         .json({ message: "Invalid data, check all fields and try again" });
     }
 
-    const index = cars.findIndex((c) => (c.id === id));
+    const index = cars.findIndex((c) => c.id === id);
 
     if (index === -1) {
       return res.status(404).json({ message: "Car not found!" });
@@ -95,15 +165,25 @@ export const updateCar = async (req: Request, res: Response) => {
       id,
       brand,
       model,
-      year,
-      type
+      yearRange,
+      bodyType,
+      horsepower,
+      torque,
+      transmission,
+      drivetrain,
+      fuelEconomy,
+      doors,
+      price,
+      engine,
+      cylinders,
     };
 
     cars[index] = updatedCar;
     await saveCars(cars);
     return res.status(200).json(updatedCar);
-
-    } catch (error) {
-    res.status(500).json({ message: "Something went wrong, check fields and try again" })
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong, check fields and try again",
+    });
   }
-} 
+};
--- src/models/Car.ts
+++ src/models/Car.ts
@@ -1,8 +1,16 @@
 export interface Car {
-    id: string,
-    brand: string,
-    model: string,
-    year: number,
-    type: string
+  id: string;
+  brand: string;
+  model: string;
+  yearRange: string;
+  bodyType: string;
+  horsepower: string;
+  torque: string;
+  transmission: string;
+  drivetrain: string;
+  fuelEconomy: string;
+  doors: string;
+  price: string;
+  engine: string;
+  cylinders: string;
 }
-    
