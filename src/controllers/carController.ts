585851e0de711ec48d7c75a2dad0ff6d4ba32aeb
Simon <simonmansson@live.se>
2025-04-21 13:55:36 +0200

    update comments

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -5,26 +5,26 @@ import { Car } from "../models/Car";
 
 let cars: Car[] = [];
 
-// GET endpoint för att hämta alla bilar.
+// GET endpoint för att HÄMTA ALLA BILAR.
 export const getAllCars = async (req: Request, res: Response) => {
   const cars = await loadCars();
   res.status(200).json(cars);
 };
 
-// GET endpoint för att hämta specifik bil.
+// GET endpoint för att HÄMTA SPECIFIK BIL.
 export const getCarById = async (req: Request, res: Response) => {
   const cars = await loadCars();
   const { id } = req.params;
   const car = cars.find((c) => c.id === id);
 
   if (!car) {
-    res.status(404).json({ message: "Car not found!" });
+   return res.status(404).json({ message: "Car not found!" });
   }
 
   return res.status(200).json(car);
 };
 
-// POST endpoint för att lägga till bil.
+// POST endpoint för att LÄGGA TILL BIL.
 export const addNewCar = async (req: Request, res: Response) => {
   const cars = await loadCars();
   const { brand, model, year, type } = req.body;
@@ -63,7 +63,7 @@ export const addNewCar = async (req: Request, res: Response) => {
   }
 };
 
-// PUT endpoint för att uppdatera/ändra en bil
+// PUT endpoint för att UPDDATERA/ÄNDRA EN SPECIFIK BIL.
 export const updateCar = async (req: Request, res: Response) => {
   const cars = await loadCars();
   const { id } = req.params;
